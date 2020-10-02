28f6d88da97ba2afe65d35efad9da6f192b3a6a4
o2j <didlsqks5@naver.com>
2020-10-02 22:46:20 +0900

    Update on Local Master Branch

--- main.py
+++ main.py
@@ -16,5 +16,9 @@ print("Hello World 4 ")
 # adding new item in master branch
 print("Hello World 5 ")
 
+#Updates in local master branch
+print("Hello World Updates on Local Mster Branch ")
+
+
 # Updates on mainbranch of GitHub
 print("Hello World Updates on GitHub")
